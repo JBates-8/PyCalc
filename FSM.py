addd140a2580b7a768aa0dac2e43f47d164ad773
osiris97 <39896719+osiris97@users.noreply.github.com>
2019-06-08 18:09:06 -0400

    Finite State Machine

---
+++ FSM.py
@@ -0,0 +1,66 @@
+"""
+File: FSM.py
+Author: Jackson Bates
+Created: 6/7/2019 7:55 PM 
+"""
+
+
+class FSM:
+
+    def __init__(self, setup_file):
+        self.setup(setup_file)
+
+
+    def setup(self, fn):
+
+        with open(fn) as f:
+            lines = [l.strip() for l in f.readlines()]
+            N_States = int(lines[0])
+            Accept_States = [int(x) for x in lines[1].split()]
+            self.generate_states(N_States,Accept_States)
+            for i in range(2,len(lines)):
+                cur = lines[i]
+                trans,details = cur.split(":")
+                s1,s2 = [int(x) for x in trans.split(">")]
+                chars,action = details.split(";")
+                self.states[s1].transitions[chars] = [s2,action]
+
+    def generate_states(self, n, acc):
+        self.states = []
+        for i in range(n):
+            if i in acc:
+                s = State(True)
+            else:
+                s = State(False)
+            self.states.append(s)
+
+
+
+
+class State:
+
+    ID = 0
+
+    def __init__(self, accept):
+        self.ID = State.ID
+        State.ID += 1
+        self.isAccept = accept
+        self.transitions = {}  # charset : state to go to
+
+    def __repr__(self):
+        return "S_"+str(self.ID)
+
+
+
+
+
+
+def get_ints():
+    return [int(x) for x in input().strip().split(" ")]
+
+
+if __name__ == '__main__':
+    fsm = FSM("fsm_setup.txt")
+    print(fsm.states)
+    for s in fsm.states:
+        print(s.ID,s.isAccept,s.transitions)
